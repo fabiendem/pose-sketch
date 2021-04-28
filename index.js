85f5f51eafbec3e201807ed9c31bffff2be96b77
Fabien Demangeat <fabien@hingehealth.com>
2021-04-28 18:39:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,198 @@
+let video;
+let poseNet;
+let poses = [];
+let previousDrawingKeypoint;
+let drawingCanvas;
+
+const PART_RIGHT_WRIST = "rightWrist";
+const PART_LEFT_WRIST = "leftWrist";
+const PART_RIGHT_EAR = "rightEar";
+const PART_LEFT_EAR = "leftEar";
+
+const DRAWING_KEYPOINT_IDENTIFIER = PART_RIGHT_WRIST;
+const NON_DRAWING_KEYPOINT = PART_LEFT_WRIST;
+
+let isDrawing = false;
+let timeDrawingToggleChanged;
+let lineColor = "#FE6B31";
+
+const stringNotDrawing = "[Not drawing]<br/>- Put your right hand at the starting position<br/>- Touch your face with your left hand to start drawing";
+const stringDrawing = "[Drawing]<br/>- Use your right hand to draw<br/>- Touch your face with your left hand to stop drawing";
+
+function setup() {
+  const canvas = createCanvas(800, 600);
+  canvas.parent("canvas-wrapper");
+
+  drawingCanvas = createGraphics(width, height);
+
+  background(102);
+
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(
+    video,
+    {
+      flipHorizontal: true,
+      minConfidence: 0.5,
+      maxPoseDetections: 1,
+      scoreThreshold: 0.5,
+      detectionType: "single",
+    },
+    modelReady
+  );
+
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on("pose", function (results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+
+  isDrawing = false;
+  select("#drawing-state").html(stringNotDrawing);
+  timeDrawingToggleChanged = millis();
+
+  select("#erase").mouseClicked(() => {
+    drawingCanvas.clear();
+  });
+  select("#download").mouseClicked(() => {
+    saveCanvas(drawingCanvas, 'myPieceOfArt.jpg');
+  });
+
+  const colorPicker = select("#color-picker");
+  colorPicker.changed(() => {
+    lineColor = colorPicker.value()
+  });
+}
+
+function modelReady() {
+  select("#loading").hide();
+}
+
+function draw() {
+  push();
+  translate(width, 0);
+  scale(-1, 1);
+  image(video, 0, 0, width, height);
+  pop();
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  //   drawSkeleton();
+  if (isDrawing) {
+    drawLineFromDrawingKeypoint();
+  }
+
+  image(drawingCanvas, 0, 0, width, height);
+
+  detectDrawingToggle();
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints() {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i += 1) {
+    // For each pose detected, loop through all the keypoints
+    const pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j += 1) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      const keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i += 1) {
+    const skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j += 1) {
+      const partA = skeleton[j][0];
+      const partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(
+        partA.position.x,
+        partA.position.y,
+        partB.position.x,
+        partB.position.y
+      );
+    }
+  }
+}
+
+function drawLineFromDrawingKeypoint() {
+  for (let i = 0; i < poses.length; i += 1) {
+    const pose = poses[i].pose;
+    const drawingKeypoint = pose[DRAWING_KEYPOINT_IDENTIFIER];
+
+    if (drawingKeypoint && drawingKeypoint.confidence > 0.55) {
+      if (previousDrawingKeypoint) {
+        drawingCanvas.stroke(lineColor);
+        drawingCanvas.strokeWeight(10);
+        drawingCanvas.line(
+          previousDrawingKeypoint.x,
+          previousDrawingKeypoint.y,
+          drawingKeypoint.x,
+          drawingKeypoint.y
+        );
+      }
+
+      previousDrawingKeypoint = drawingKeypoint;
+    }
+  }
+}
+
+function detectDrawingToggle() {
+    const timeSinceDrawingToggleChanged = millis() - timeDrawingToggleChanged;
+    console.log(timeSinceDrawingToggleChanged);
+    if(timeSinceDrawingToggleChanged < 2 * 1000) {
+        return;
+    }
+    for (let i = 0; i < poses.length; i += 1) {
+        const pose = poses[i].pose;
+        const nonDrawingKeypoint = pose[NON_DRAWING_KEYPOINT];
+        const leftEar = pose[PART_LEFT_EAR];
+        const rightEar = pose[PART_RIGHT_EAR];
+
+        if (!nonDrawingKeypoint || (!leftEar && !rightEar)) {
+            return;
+        }
+
+        if(nonDrawingKeypoint.confidence < 0.5) {
+            return;
+        }
+
+        let minDistFromEars = 999999999999;
+        if (leftEar) {
+            const distFromLeftEar = Math.sqrt(Math.pow(leftEar.x - nonDrawingKeypoint.x, 2) - Math.pow(leftEar.y - nonDrawingKeypoint.y, 2));
+            if(!isNaN(distFromLeftEar)) {
+                minDistFromEars = distFromLeftEar < minDistFromEars ? distFromLeftEar : minDistFromEars;
+            }
+        }
+        if(rightEar) {
+            const distFromRightEar = Math.sqrt(Math.pow(rightEar.x - nonDrawingKeypoint.x, 2) - Math.pow(rightEar.y - nonDrawingKeypoint.y, 2));
+            if(!isNaN(distFromRightEar)) {
+                minDistFromEars = distFromRightEar < minDistFromEars ? distFromRightEar : minDistFromEars;
+            }
+        }
+        if(minDistFromEars < 20) {
+            console.log("Touching face");
+            timeDrawingToggleChanged = millis();
+            isDrawing = !isDrawing;
+
+            select("#drawing-state").html(isDrawing ? stringDrawing : stringNotDrawing);
+
+        }
+        
+      }
+}
